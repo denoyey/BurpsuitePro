b11e01adc263ddf9d6fc411265e91c5254a566bf
denoyey <110691104+denoyey@users.noreply.github.com>
2025-08-19 09:36:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
-# Instalasi Dependencies
-echo "Proses menginstall dependencies..."
+# Installing dependencies
+echo -e "\n[*] Installing dependencies..."
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
 sudo apt install git axel openjdk-21-jre openjdk-22-jre openjdk-23-jre -y
 
-# Mengcloning repository github
+# Cloning GitHub repository
+echo -e "\n[*] Cloning GitHub repository..."
 git clone https://github.com/denoyey/BurpsuitePro.git
 cd BurpsuitePro
 
-# Mendownload BurpsuitePro Terbaru
-echo "Proses mendownload BurpSuitePro versi terbaru..."
+# Downloading latest BurpSuitePro
+echo -e "\n[*] Downloading latest BurpSuitePro..."
 version=2025.7.3
 url="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar&version=$version"
 axel "$url" -o "burpsuite_pro_v$version.jar"
 
-# Menjalankan Kunci Lisensi BurpsuitePro
-echo "Menjalankan loader lisensi BuprpsuitePro..."
+# Starting license loader
+echo -e "\n[*] Starting license loader..."
 (java -jar loader.jar) &
 
-# Menjalankan BurpsuitePro
-echo "Menjalankan BurpsuitePro..."
-echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
+# Creating launcher
+echo -e "\n[*] Creating launcher..."
+cat <<EOF > burpsuitepro
+#!/bin/bash
+java \\
+  --add-opens=java.desktop/javax.swing=ALL-UNNAMED \\
+  --add-opens=java.base/java.lang=ALL-UNNAMED \\
+  --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED \\
+  --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED \\
+  --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED \\
+  -javaagent:\$(pwd)/loader.jar \\
+  -noverify \\
+  -jar \$(pwd)/burpsuite_pro_v$version.jar &
+EOF
+
 chmod +x burpsuitepro
-cp burpsuitepro /bin/burpsuitepro
-(./burpsuitepro)
+sudo cp burpsuitepro /bin/burpsuitepro
+
+# Launching BurpSuitePro
+echo -e "\n[*] Launching BurpSuitePro..."
+./burpsuitepro
