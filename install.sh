0e61da48f9b97102f4aefa584e0f04664fb017be
denoyey <110691104+denoyey@users.noreply.github.com>
2025-08-19 09:24:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,14 +11,7 @@ cd BurpsuitePro || { echo "[!] Directory not found."; exit 1; }
 
 # Mendownload BurpsuitePro Terbaru. 
 echo "\n[*] Detecting latest Burp Suite Pro version..."
-version=$(curl -s 'https://portswigger.net/burp/releases/professional?requestedplatform=linux' | grep -Po 'download\?product=pro&type=Jar&version=\K[0-9]+\.[0-9]+\.[0-9]+' | head -n1)
-if [[ -z "$version" ]]; then
-  echo "[!] Gagal mendeteksi versi terbaru BurpSuite."
-  exit 1
-fi
-echo "\n[*] Latest version detected: $latest_version"
-url="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar&version=$version"
-axel "$url" -o "burpsuite_pro_v$version.jar"
+version=2025.5.8 url="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar&version=$version" axel "$url" -o "burpsuite_pro_v$version.jar"
 
 # Menjalankan Kunci Lisensi BurpsuitePro
 echo "\n[*] Starting license loader..."
@@ -26,10 +19,4 @@ echo "\n[*] Starting license loader..."
 
 # Menjalankan BurpsuitePro 
 echo "\n[*] Creating launcher script..."
-echo "java 
---add-opens=java.desktop/javax.swing=ALL-UNNAMED 
---add-opens=java.base/java.lang=ALL-UNNAMED 
---add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED 
---add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED 
---add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED 
--javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro chmod +x burpsuitepro cp burpsuitepro /bin/burpsuitepro (./burpsuitepro)
+echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro chmod +x burpsuitepro cp burpsuitepro /bin/burpsuitepro (./burpsuitepro)
