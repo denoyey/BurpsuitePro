7d00503c323b601a615c9da5f72de21a8fc47bf4
denoyey <110691104+denoyey@users.noreply.github.com>
2025-08-20 06:53:55 +0700

    Update run.bash

--- run.bash
+++ run.bash
@@ -1,16 +1,24 @@
 #!/bin/bash
 # Github: github.com/denoyey/BurpsuitePro.git
 
+
 os_type=$(uname | tr '[:upper:]' '[:lower:]')
 
 case "$os_type" in
-  linux*)
-    echo -e "\nDetected Linux OS. Running Linux installer..."
+  linux*|darwin*)
+    echo -e "\nDetected Unix-like OS. Running Linux/macOS installer..."
     bash burp-files/os/linux/install.sh
     ;;
   mingw*|cygwin*|msys*)
     echo -e "\nDetected Windows OS. Running Windows installer..."
-    pwsh -File burp-files/os/windows/install.ps1
+    if command -v pwsh &>/dev/null; then
+      pwsh -NoProfile -ExecutionPolicy Bypass -File burp-files/os/windows/install.ps1
+    elif command -v powershell &>/dev/null; then
+      powershell -NoProfile -ExecutionPolicy Bypass -File burp-files/os/windows/install.ps1
+    else
+      echo "PowerShell not found. Cannot continue."
+      exit 1
+    fi
     ;;
   *)
     echo "Unsupported OS: $os_type"
