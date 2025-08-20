8540b2f8e2f428dabb6c37522322ffa8f2717ea4
denoyey <110691104+denoyey@users.noreply.github.com>
2025-08-20 08:23:27 +0700

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import os
 import platform
 import subprocess
 import sys
+import shutil
 
 RED = "\033[91m"
 GREEN = "\033[92m"
@@ -14,13 +15,31 @@ BLUE = "\033[94m"
 CYAN = "\033[96m"
 RESET = "\033[0m"
 
+
+def check_python_installed():
+    python_exec = shutil.which("python") or shutil.which("python3")
+    if not python_exec:
+        print(f"{RED}[X]{RESET} Python is not installed or not found in PATH.")
+        print(f"{YELLOW}[!] Please install Python 3.x and try again.{RESET}")
+        sys.exit(1)
+    else:
+        version = subprocess.check_output([python_exec, "--version"]).decode().strip()
+        print(f"{GREEN}[DONE]{RESET} Python detected: {version}")
+
+
 try:
+    check_python_installed()
     os_type = platform.system().lower()
     if "microsoft" in platform.release().lower():
         os_type = "wsl"
     print(f"\n{CYAN}Detected OS:{RESET} {YELLOW}{os_type}{RESET}")
     if "windows" in os_type:
         print(f"\n{BLUE}[*]{RESET} Running Windows installer...")
+        if shutil.which("powershell") is None:
+            print(
+                f"{RED}[X] PowerShell not found in PATH. Please ensure PowerShell is installed and available.{RESET}"
+            )
+            sys.exit(1)
         try:
             subprocess.run(
                 [
@@ -50,5 +69,5 @@ except KeyboardInterrupt:
     Github: github.com/denoyey/BurpsuitePro.git
     """
     print(ascii_art)
-    print(f"{RED}[!] Installation cancelled by user.{RESET}")
+    print(f"{YELLOW}[!] Installation cancelled by user.{RESET}\n")
     sys.exit(130)
